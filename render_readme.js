eff19c54376b64f8d542221b77e8ea6f8eb40fb8
Zachary <139969581+Call-me-Zak@users.noreply.github.com>
2024-08-12 22:36:00 +0100

    Update render_readme.js

--- render_readme.js
+++ render_readme.js
@@ -1,10 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
     fetch('README.md')
         .then(response => response.text())
         .then(text => {
             document.getElementById('markdown-content').innerHTML = marked(text);
-        })
-        .catch(error => {
-            console.error('Error fetching README.md:', error);
         });
 });
