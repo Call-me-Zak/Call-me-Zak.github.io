94db0bb285ffbca29cb267760906fb235d86b928
Zachary <139969581+Call-me-Zak@users.noreply.github.com>
2024-08-12 13:33:13 +0100

    Update readme_render.js

--- readme_render.js
+++ readme_render.js
@@ -2,14 +2,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const aboutButton = document.getElementById("about-button");
 
-    // Add an event listener to the "ABOUT" button
-    aboutButton.addEventListener("click", function() {
+    // Add an event listener to the "ABOUT" link
+    aboutButton.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent default anchor behavior
+
         // Fetch the README.md file
         fetch('README.md')
             .then(response => response.text())
             .then(text => {
                 // Convert Markdown to HTML and display it
                 document.getElementById('markdown-content').innerHTML = marked(text);
+
+                // Smoothly scroll to the "about" section
+                document.querySelector('#about').scrollIntoView({
+                    behavior: 'smooth'
+                });
             })
             .catch(error => {
                 console.error('Error fetching README.md:', error);
